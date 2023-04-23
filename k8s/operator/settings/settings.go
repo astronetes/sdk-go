3e96b28edac1a0b178408d9ff9a4a5a84cccd488
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-04-23 06:55:49 +0200

    Add flat setting sfor phase

--- k8s/operator/settings/settings.go
+++ k8s/operator/settings/settings.go
@@ -16,20 +16,54 @@ type Settings struct {
 }
 
 type Controller struct {
-	Timeout       time.Duration         `json:"timeout,omitempty"`
-	MaxConditions int32                 `json:"max_conditions,omitempty"`
-	Phases        []ReconciliationPhase `json:"phases,omitempty"`
+	Timeout             time.Duration         `json:"timeout,omitempty"`
+	MaxConditions       int32                 `json:"max_conditions,omitempty"`
+	DefaultRequeueAfter *time.Duration        `json:"defaultRequeueAfter,omitempty"`
+	Phases              []ReconciliationPhase `json:"phases,omitempty"`
+}
+
+func (ctrl Controller) GetPhase(code v1.PhaseCode) ReconciliationPhase {
+	for _, p := range ctrl.Phases {
+		if p.Name == code {
+			return p
+		}
+	}
+	return ReconciliationPhase{}
 }
 
 type ReconciliationPhase struct {
-	Name    v1.PhaseCode    `json:"name,omitempty"`
-	Backoff []time.Duration `json:"backoff,omitempty"`
+	Name         v1.PhaseCode    `json:"name,omitempty"`
+	Backoff      []time.Duration `json:"backoff,omitempty"`
+	RequeueAfter *time.Duration  `json:"requeueAfter,omitempty"`
 }
 
 type Monitoring struct {
 	Address  string `json:"adress,omitempty"`
 	Username string `json:"username,omitempty"`
 	Password string `json:"password,omitempty"`
+}
+
+type PhaseSettings struct {
+	Timeout       time.Duration   `json:"timeout,omitempty"`
+	MaxConditions int32           `json:"max_conditions,omitempty"`
+	Backoff       []time.Duration `json:"backoff,omitempty"`
+	RequeueAfter  *time.Duration  `json:"requeueAfter,omitempty"`
+}
+
+func (ctrl Controller) GetSettingsBy(code v1.PhaseCode) PhaseSettings {
+	out := PhaseSettings{
+		Timeout:       ctrl.Timeout,
+		MaxConditions: ctrl.MaxConditions,
+		RequeueAfter:  ctrl.DefaultRequeueAfter,
+	}
+	phase := ctrl.GetPhase(code)
+	if phase.Backoff != nil {
+		out.Backoff = phase.Backoff
+	}
+	if phase.RequeueAfter != nil {
+		out.RequeueAfter = phase.RequeueAfter
+	}
+	return out
 }
 
 func LoadFromConfigFile(path string) (Settings, error) {
@@ -43,5 +77,4 @@ func LoadFromConfigFile(path string) (Settings, error) {
 	return settings, err
 }
 
-type ReconciliationPhaseSettings struct {
-}
+type ReconciliationPhaseSettings struct{}
