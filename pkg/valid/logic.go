8deed0287ce0f8bfca450bebb46e3967411f6bc0
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-05-03 06:41:52 +0200

    Add new function to check If only one value is not nil

---
+++ pkg/valid/logic.go
@@ -0,0 +1,19 @@
+package valid
+
+import "fmt"
+
+func OnlyOneOfThemIsInitialized(items ...interface{}) (int, error) {
+	var notNilIndex = -1
+	for i, item := range items {
+		if item != nil {
+			if notNilIndex > -1 {
+				return -1, fmt.Errorf("more than one element has been provided")
+			}
+			notNilIndex = i
+		}
+	}
+	if notNilIndex < 0 {
+		return -1, fmt.Errorf("none of the elements has been provided")
+	}
+	return notNilIndex, nil
+}
