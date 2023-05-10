16a60304fe71162c2a48b8fa04e37469020fc232
Pasquale Toscano <pasquale.toscano@astrokube.com>
2023-05-10 15:13:28 +0200

    Add method to get absolute path

--- pkg/fsys/file.go
+++ pkg/fsys/file.go
@@ -1,6 +1,8 @@
 package fsys
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/astronetes/sdk-go/internal/fsys"
@@ -10,4 +12,13 @@ func GetFileContent(path string) ([]byte, error) {
 	dirPath, filename := filepath.Split(path)
 	// TODO: The above line could not work as expected for all the filesystems
 	return fsys.GetFileContent(dirPath, filename)
+}
+
+func GetAbsoluteFilePath(relativePath string) (string, error) {
+	path, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("file://%s/%s", path, relativePath), nil
 }
